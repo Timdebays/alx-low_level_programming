7d8a38ffc2f5212e7b8985bd5d701268dc1c3336
Timdebays <timdebays@gmail.com>
2023-03-23 09:08:48 -0700

    Task 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if letter is uppercase
+ * @x: The number to be checked
+ * Return 1 if True and 0 if false
+ */
+
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
