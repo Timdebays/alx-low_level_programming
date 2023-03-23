9e39d53f5898b2de2012db1b0857f96109fd7ebe
Timdebays <timdebays@gmail.com>
2023-03-23 15:46:04 -0700

    Task 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,13 @@
 
 /**
  * more_numbers - Print 10 times the numbers since 0 up to 14
- * Return: 10 times of the numbers since 0 to 14
+ * Return: 10 times of the numbers since 0 up to 14
  */
 
 void more_numbers(void)
 
 {
+
 	int x, y;
 
 	for (x = 0; x < 10; x++)
@@ -16,10 +17,10 @@ void more_numbers(void)
 	{
 	if (y > 9)
 	{
-	_putchar((y/10) + '0')
+	_putchar((y/10) + '0');
 	}
-	_putchar((y/14) + '0')
+	_putchar((y/14) + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 	}
 }
