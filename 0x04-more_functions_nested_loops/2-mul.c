54f7faccd5724eaaeb52f06d247c6d8e4438eb37
Timdebays <timdebays@gmail.com>
2023-03-23 09:49:29 -0700

    Task 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _mul - multiply two integers
+ * @x, y: The two  numbers to multiply
+ * Return: result
+ */
+
+int _mul(int x, int y)
+
+{
+        int result;
+
+	result = x * y
+        return result;
+        
+}
