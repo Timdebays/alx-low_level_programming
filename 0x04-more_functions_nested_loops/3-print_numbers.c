dbd642caa929ce1ae6edf9cf128a9b382f75e461
Timdebays <timdebays@gmail.com>
2023-03-23 10:14:34 -0700

    Task 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - print number 0 to 9
+ * Return: The numbers
+ */
+
+void print_numbers(void)
+
+{
+        for (x = 0; x <= 9; x++)
+        {
+        _putchar(x + '0');
+        }
+        _putchar('\n');
+}
