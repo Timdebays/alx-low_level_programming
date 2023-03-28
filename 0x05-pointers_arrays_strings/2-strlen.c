432b92655a0262909be500246bbde0f93a0946b1
Timdebays <timdebays@gmail.com>
2023-03-28 13:46:44 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen()- To know the length of string
+ *@s : the pointer s
+ *Return :the length of the pointer s
+ */
+
+int _strlen(char *s)
+
+{
+	_strlen(s);
+	return (0);
+}
